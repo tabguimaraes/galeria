a6a5c8a552008b851661220ded3a4116860eef16
Tiago Guimaraes <tab.guimaraes@gmail.com>
2025-08-13 20:58:30 -0300

    ajustes gerais, exclusão de comentarios, etc

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,10 +10,6 @@ async function getImages() {
   try {
     const img = await fetch(`https://picsum.photos/v2/list?page=${pagina}&limit=10`);
     let searchResponse = await img.json().catch();
-    // Código console.log está funcionando
-    // console.log(searchResponse[0].download_url);
-
-    // searchResponse;
 
     searchResponse.forEach((item) => {
       imgAuthor = item.author;
@@ -40,59 +36,6 @@ function setImages(author, url) {
   picture.appendChild(span);
 }
 
-// const imgPath = [
-//   {
-//     url: "./assets/img/erik-mclean-pHUfHCAEyTU-unsplash.jpg",
-//     nome: "Imagem 1",
-//   },
-//   {
-//     url: "./assets/img/evan-clark-2oG4ynkv880-unsplash.jpg",
-//     nome: "Imagem 2",
-//   },
-//   {
-//     url: "./assets/img/erik-mclean-qkEc13VaPRA-unsplash.jpg",
-//     nome: "Imagem 3",
-//   },
-
-//   {
-//     url: "./assets/img/jonathan-cooper-kpOGJ2bma4o-unsplash.jpg",
-//     nome: "Imagem 4",
-//   },
-//   {
-//     url: "./assets/img/luemen-rutkowski-EKa3EPL37Dc-unsplash.jpg",
-//     nome: "Imagem 5",
-//   },
-//   {
-//     url: "./assets/img/sonny-mauricio-CBYLA4GK4qk-unsplash.jpg",
-//     nome: "Imagem 6",
-//   },
-//   {
-//     url: "./assets/img/oscar-garcia-castillo-ZMFbova77nI-unsplash.jpg",
-//     nome: "Imagem 7",
-//   },
-//   {
-//     url: "./assets/img/samuel-ng-__5V3g2mhTE-unsplash.jpg",
-//     nome: "Imagem 8",
-//   },
-// ];
-
-// function createIMG() {
-//   imgPath.forEach((imagem) => {
-//     const img = document.createElement("img");
-//     const picture = document.createElement("picture");
-//     const span = document.createElement("span");
-//     picture.classList.add("galeria-item");
-//     img.src = imagem.url;
-//     img.alt = imagem.nome;
-//     span.innerText = imagem.nome;
-//     galeriaContainer.appendChild(picture);
-//     picture.appendChild(img);
-//     picture.appendChild(span);
-//   });
-// }
-
-// createIMG();
-
 window.addEventListener("scroll", () => {
   const scrollY = window.scrollY;
   const windowHeight = window.innerHeight;
@@ -103,7 +46,6 @@ window.addEventListener("scroll", () => {
   if (PosicaoScroll > 50) {
     pagina++;
     PosicaoScroll = 0;
-    // createIMG();
     getImages();
   }
 });
